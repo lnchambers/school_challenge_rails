b271b67dc9fa52872e64b28c051d0abbfbb2172f
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 14:06:47 -0700

    Add FactoryBot and DatabaseCleaner

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Student, type: :model do
 
       expect(student).to be_invalid
     end
+
+    it "is valid with a name" do
+      student = create(:student)
+
+      expect(student).to be_valid
+    end
   end
 end
