2d1a3fa4796a8b189d1b56ac12ccc94eb52c513a
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 16:16:16 -0700

    Fix last bug

--- spec/features/courses/user_can_see_all_students_on_the_show_page_spec.rb
+++ spec/features/courses/user_can_see_all_students_on_the_show_page_spec.rb
@@ -8,6 +8,7 @@ describe "As a user" do
       course.students << student
 
       visit course_path(course)
+      save_and_open_page
 
       expect(page).to have_content(student.name)
       expect(page).to have_content(course.name)
