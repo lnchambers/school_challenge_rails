58f595de8a1e423f495c3ce6db8bdebf953a028d
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 15:28:08 -0700

    Build out test for addresses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :students, shallow: true do
-    resources :addresses, only: [:create]
+    resources :addresses, only: [:create, :index]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/addresses/user_can_create_a_new_address_spec.rb
+++ spec/features/addresses/user_can_create_a_new_address_spec.rb
@@ -2,6 +2,23 @@ require "rails_helper"
 
 describe "As a user" do
   describe "when I visit students/:id/addresses/new" do
+    it "I can create a new address" do
+      student = create(:student)
+      visit student_addresses_path(student)
 
+      fill_in "address[description]", with: "A sunny side up"
+      fill_in "address[street]", with: "123 Egg Way"
+      fill_in "address[city]", with: "Oval"
+      fill_in "address[state]", with: "Of Being"
+      fill_in "address[zip]", with: 38838
+      click_on "Create Address"
+
+      expect(current_path).to eq(student_path(student))
+      expect(page).to have_content("A sunny side up")
+      expect(page).to have_content("123 Egg Way")
+      expect(page).to have_content("Oval")
+      expect(page).to have_content("Of Being")
+      expect(page).to have_content("38838")
+    end
   end
 end
