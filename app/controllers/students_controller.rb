c3216a2b992b0a4b4d5fbeed63997afa70fee334
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 14:46:02 -0700

    Delete functionality complete

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -35,6 +35,12 @@ class StudentsController < ApplicationController
     end
   end
 
+  def destroy
+    student = Student.find(params[:id])
+    student.destroy
+    redirect_to students_path
+  end
+
   private
 
   def student_params
