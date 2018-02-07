7156122b3ba0c6efd79248e95e51e1ea5ee31c78
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 15:50:05 -0700

    Work on join table for courses and students

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180207224149) do
+ActiveRecord::Schema.define(version: 20180207224926) do
 
   create_table "addresses", force: :cascade do |t|
     t.text "description"
@@ -24,10 +24,24 @@ ActiveRecord::Schema.define(version: 20180207224149) do
     t.index ["student_id"], name: "index_addresses_on_student_id"
   end
 
+  create_table "classes", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_classes_on_course_id"
+    t.index ["student_id"], name: "index_classes_on_student_id"
+  end
+
   create_table "courses", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
+    t.index ["course_id"], name: "index_courses_students_on_course_id"
+    t.index ["student_id"], name: "index_courses_students_on_student_id"
   end
 
   create_table "students", force: :cascade do |t|
