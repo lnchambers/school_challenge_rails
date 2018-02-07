cbea03811345d43c3fff9658c65bcf9214940a22
Luke Chambers <lukenatchambers@gmail.com>
2018-02-07 15:40:02 -0700

    Addresses are functional

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :students, shallow: true do
-    resources :addresses, only: [:create, :index]
+    resources :addresses, only: [:create, :new]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/addresses/user_can_create_a_new_address_spec.rb
+++ spec/features/addresses/user_can_create_a_new_address_spec.rb
@@ -4,7 +4,7 @@ describe "As a user" do
   describe "when I visit students/:id/addresses/new" do
     it "I can create a new address" do
       student = create(:student)
-      visit student_addresses_path(student)
+      visit new_student_address_path(student)
 
       fill_in "address[description]", with: "A sunny side up"
       fill_in "address[street]", with: "123 Egg Way"
